83bb04f2b533492702ca3cb269a086c490f6ed82
b6dbde <b6dbde@gmail.com>
2024-06-18 22:28:19 +0700

    adding service container binding rules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,15 @@ class AppServiceProvider extends ServiceProvider
             ->needs(AuthService::class)
             ->give(fn () => new AuthService(new Customer()));
 
+        $this->app->when(\App\Http\Controllers\CustomerController::class)
+            ->needs(AuthService::class)
+            ->give(fn () => new AuthService(new Customer()));
+
         $this->app->when(\App\Http\Controllers\Admin\AuthController::class)
+            ->needs(AuthService::class)
+            ->give(fn () => new AuthService(new Admin()));
+
+        $this->app->when(\App\Http\Controllers\Admin\AdminController::class)
             ->needs(AuthService::class)
             ->give(fn () => new AuthService(new Admin()));
     }
